e0013cb08d9f175cc0bc5344af2fc9a8deef8c37
sakibzaman1 <sakibzaman20@gmail.com>
2023-11-06 12:59:20 +0600

    single service details page has been added

--- src/Pages/AddServices/AddServices.jsx
+++ src/Pages/AddServices/AddServices.jsx
@@ -12,13 +12,15 @@ const AddServices = () => {
         const serviceName = form.serviceName.value;
         const serviceImage = form.serviceImage.value;
         const price = form.price.value;
+        const providerLocation = form.providerLocation.value;
+        const serviceArea = form.serviceArea.value;
         const proName = user?.displayName;
         const proImage = user?.photoURL;
         const proEmail = user?.email;
         const description = form.description.value;
         // console.log(serviceName, serviceImage, price, proName, proImage, proEmail, description)
 
-        const newService = {serviceName, serviceImage, price, proName, proImage, proEmail, description};
+        const newService = {serviceName, serviceImage, price, proName, proImage, proEmail,providerLocation, serviceArea, description};
         console.log(newService);
          // send data to the server
 
@@ -36,7 +38,7 @@ const AddServices = () => {
                 swal({
                     position: 'top-center',
                     icon: 'success',
-                    title: 'Product Added Successfully',
+                    title: 'Service Added Successfully',
                     showConfirmButton: false,
                     showCancelButton: false,
                     timer: 2000
@@ -47,7 +49,7 @@ const AddServices = () => {
 
     return (
         <div>
-            <section className="bg-white dark:bg-gray-900">
+            <section className="bg-slate-200 dark:bg-gray-900 mt-10">
   <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
       <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new service</h2>
       <form onSubmit={handleAddService}>
@@ -64,7 +66,7 @@ const AddServices = () => {
                   <label for="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service Price</label>
                   <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required=""/>
               </div>
-              <div className='sm:col-span-2'>
+              <div>
                   <label for="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Provider's Name</label>
                   <input defaultValue={user?.displayName} readOnly type='text' name='proName' placeholder='Type Provider Name' id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                   </input>
@@ -77,13 +79,21 @@ const AddServices = () => {
                   <label for="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Provider's Image URL</label>
                   <input defaultValue={user?.photoURL} readOnly type="text" name="providerImage" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Providers image URL" required=""/>
               </div>
+              <div className="w-full">
+                  <label for="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service Area</label>
+                  <input type="text" name="serviceArea" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Service Area" required=""/>
+              </div>
+              <div className="sm:col-span-2">
+                  <label for="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Provider Location</label>
+                  <input type="text" name="providerLocation" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Provider Location" required=""/>
+              </div>
               <div className="sm:col-span-2">
                   <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
                   <textarea name='description' id="description" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
               </div>
           </div>
-          <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-black bg-teal-600 p-4 bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-              Add product
+          <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-teal-600 p-4 bg-primary-700 rounded-none focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+              Add Service
           </button>
       </form>
   </div>
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -7,6 +7,7 @@ import Registration from '../Pages/Registration/Registration';
 import '../Router/router.css'
 import AddServices from '../Pages/AddServices/AddServices';
 import Services from '../Components/Services/Services';
+import ServiceDetails from '../Components/Services/ServiceDetails';
 
 const router = createBrowserRouter([
     {
@@ -30,6 +31,11 @@ const router = createBrowserRouter([
                 path: '/services',
                 element: <Services></Services>,
                 loader: ()=> fetch(`http://localhost:5000/services`)
+            },
+            {
+                path: '/servicedetails/:id',
+                element: <ServiceDetails></ServiceDetails>,
+                loader: ({params})=> fetch(`http://localhost:5000/services/${params.id}`)
             },
             {
                 path: '/addservices',
